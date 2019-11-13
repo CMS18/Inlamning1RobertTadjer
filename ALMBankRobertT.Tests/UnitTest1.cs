7f270d5ab4ed48d9cc781ca8b05060698f8e44a4
roberttadjer <robert.tadjer@gmail.com>
2019-11-13 14:46:00 +0100

    Add project files.

---
+++ ALMBankRobertT.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ALMBankRobertT.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# ALM course project in ASP.NET Core 2.2
+
+**By:** Robert Tadjer
+
+**Code review by:** Samie Shaya

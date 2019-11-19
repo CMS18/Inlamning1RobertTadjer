036ad04fdc2588a9a8cabbef8d90ec89b333860d
Samie <samieshaya@gmail.com>
2019-11-19 21:11:32 +0100

    Feature: överföringar (#3)

---
+++ ALMBankRobertT.App/Models/ViewModels/TransferViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ALMBankRobertT.App.Models.ViewModels
+{
+    public class TransferViewModel
+    {
+        [Required]
+        [Display(Name = "Summa")]
+        public decimal Amount { get; set; }
+
+        [Required]
+        [Display(Name = "Kontot du för över pengar från")]
+        public int TransferFromId { get; set; }
+
+        [Required]
+        [Display(Name = "Kontot du för över pengar till")]
+        public int TransferToId { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        public string SuccessMessage { get; set; }
+    }
+}
